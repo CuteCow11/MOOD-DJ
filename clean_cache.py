b122dc6acd8fd7b2f4205d8c0543f6edb69aec1c
CuteCow11 <krishshukla.11052006@gmail.com>
2025-12-18 22:07:11 +0530

    Update clean_cache.py

--- clean_cache.py
+++ clean_cache.py
@@ -2,12 +2,12 @@ import os
 
 print("Searching for Spotify cache files...")
 
-# Common names for the cache file
+
 files_to_remove = [".spotipy_cache", ".cache", ".spotify_cache"]
 
 found = False
 for filename in os.listdir('.'):
-    # Check if the file starts with .cache or matches our list
+
     if filename.startswith(".cache") or filename in files_to_remove:
         try:
             os.remove(filename)
